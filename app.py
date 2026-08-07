1e7777bf0c81a7b954f7544a79d01c257709ffac
Gracy Mahajan <gracymahajan09022005@gmail.com>
2026-08-07 22:31:37 +0530

    feat: made config.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from dotenv import load_dotenv
-import os
-from google import genai
-
-load_dotenv()
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+from config import client
 history = []
 while True:
     question = input("You: ")
